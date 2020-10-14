19e48975765902390d986d505121b9631e4bd6ad
Mateusz Kulinowski <mat.kulinowski@gmail.com>
2020-10-14 17:07:53 +0200

    add threads

--- testASM/Program.cs
+++ testASM/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace testASM
 {
@@ -10,32 +10,55 @@ namespace testASM
     {
         static void Main(string[] args)
         {
-            //C#
             Bitmap bmp = new Bitmap(@"D:\studia\Gildia Magów Ognia\JA\projekt\aei.bmp");
-
-            BitmapData data = bmp.LockBits(new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-            int stride = data.Stride;
-            bmp.UnlockBits(data);
-
-            //int bitsPerPixel = ((int)bmp.PixelFormat & 0xff00) >> 8;
-            //int bytesPerPixel = (bitsPerPixel + 7) / 8;
-            //var stride2 = 4 * ((bmp.Width * bytesPerPixel + 3) / 4);
+            int threadCount = 4;
+            int stride = GetStride(bmp);
+            byte red = 200, green = 100, blue = 50;
+            bool useASM = false;
 
             byte[] inByte = ImageToByte(bmp);
             byte[] resultByte = ImageToByte(bmp);
 
-            CsDll.Filter.AddFilterCs(ref resultByte, inByte, 0.5, 54, bmp.Width*bmp.Height*3, 120, 200, 50, bmp.Width, stride);
-            File.WriteAllBytes("wynik.bmp", resultByte);
-
-
-            //ASM
-            unsafe
+            Thread[] threads = new Thread[threadCount];
+            switch (useASM)
             {
-                ASM.AddFilterASM(2.0, 3.0);
+                case true:
+                    for (int i = 0; i < threadCount; i++)
+                    {
+                        unsafe
+                        {
+                            ASM.AddFilterASM(2.0, 3.0);
+                        }
+                    }
+                    File.WriteAllBytes("wynik.bmp", resultByte);
+                    break;
+
+                case false:
+                    for (int i = 0; i < threadCount; i++)
+                    {
+                        threads[i] = new Thread(() =>
+                        CsDll.Filter.AddFilterCs(
+                            ref resultByte,
+                            inByte,
+                            0.5,
+                            54,
+                            bmp.Width * bmp.Height * 3,
+                            red, green, blue,
+                            bmp.Width,
+                            stride));
+                    }
+                    File.WriteAllBytes("wynik.bmp", resultByte);
+                    break;
             }
         }
 
-
+        public static int GetStride(Bitmap bmp)
+        {
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            int stride = data.Stride;
+            bmp.UnlockBits(data);
+            return stride;
+        }
 
         public static byte[] ImageToByte(Image img)
         {
@@ -46,10 +69,12 @@ namespace testASM
             }
         }
     }
+
     class ASM
     {
         [DllImport(@"C:\Users\CLEVO\source\repos\JA-ASM\x64\Debug\AsmDll.dll")]
         public static unsafe extern void AddFilterASM(double a, double b);
     }
 }
+
 
